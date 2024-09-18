4a73c73e7c7658973d6e19163720fa113fe738fd
Marcel Ezenagu <marcelezenagu92@gmail.com>
2024-09-18 14:17:29 +0100

    fix: includes country and exchangeRate endpoints

--- src/module/country_currency/dto/update-country_currency.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCountryCurrencyDto } from './create-country_currency.dto';
-
-export class UpdateCountryCurrencyDto extends PartialType(CreateCountryCurrencyDto) {}
