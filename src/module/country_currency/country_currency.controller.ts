61eec22693f1a3d7bcb8c52d3b161cdd3915af25
Marcel Ezenagu <marcelezenagu92@gmail.com>
2024-09-18 14:25:13 +0100

    fix: includes country and exchangeRate endpoints

--- src/module/country_currency/country_currency.controller.ts
+++ src/module/country_currency/country_currency.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CountryCurrencyService } from './country_currency.service';
-import { CreateCountryCurrencyDto } from './dto/create-country_currency.dto';
-import { UpdateCountryCurrencyDto } from './dto/update-country_currency.dto';
 
 @Controller('country-currency')
 export class CountryCurrencyController {
