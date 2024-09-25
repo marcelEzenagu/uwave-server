1cdad20e77b933b7136772e04bae2202c0076157
Marcel Ezenagu <marcelezenagu92@gmail.com>
2024-09-25 12:55:01 +0100

    fix:includes image size limit

--- src/helpers/upload.ts
+++ src/helpers/upload.ts
@@ -5,8 +5,25 @@ import * as path from 'path';
 @Injectable()
 export class FileService {
   async uploadImage(img_string: string, dir: string, filename: string): Promise<boolean> {
-    // Extract base64 part of the image
+    
     const base64Image = img_string.split(';base64,').pop();
+  
+    if (!base64Image) {
+      throw new HttpException('Invalid image string', HttpStatus.BAD_REQUEST);
+    }
+  
+    // Calculate the size of the base64 image string
+    const imageSizeInBytes = (base64Image.length * 3) / 4 - (base64Image.endsWith('==') ? 2 : base64Image.endsWith('=') ? 1 : 0);
+  
+    const MAX_SIZE_IN_BYTES = Number(process.env.IMAGE_SIZE_LIMIT) // 1MB
+  
+    // Check if the image exceeds the maximum allowed size
+    if (imageSizeInBytes > MAX_SIZE_IN_BYTES) {
+      throw new HttpException('Image exceeds 1MB size limit', HttpStatus.PAYLOAD_TOO_LARGE);
+    }
+    
+    // // Extract base64 part of the image
+    // const base64Image = img_string.split(';base64,').pop();
 
     // Make the directory path if it doesn't exist
     const dirPath = path.resolve(dir);
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './helpers/exception';
-import { join } from 'path';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({}));
   app.useGlobalFilters(new AllExceptionsFilter());
-  // app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.use(bodyParser.json({ limit: '10mb' }));
 
   app.enableCors({
     origin:true,
