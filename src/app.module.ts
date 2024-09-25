fe2e9dd28f71da629d9ebbed444755438808d116
Marcel Ezenagu <marcelezenagu92@gmail.com>
2024-09-25 08:46:28 +0100

    fix:includes image serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './module/user/user.module';
 import { VendorModule } from './module/vendor/vendor.module';
@@ -10,7 +10,7 @@ import { CartModule } from './module/cart/cart.module';
 import { LogisticsModule } from './module/logistics/logistics.module';
 import forFeatureDb from './db/for-feature.db';
 import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from './helpers/new'
+import { AllExceptionsFilter } from './helpers/new';
 import { HealthController } from './health/health.controller';
 import { SavedItemsModule } from './module/saved-items/saved-items.module';
 import { UWaveUserModule } from './module/u-wave-user/u-wave-user.module';
@@ -25,26 +25,54 @@ import { CountryCurrencyModule } from './module/country_currency/country_currenc
 import { ProductCategoryModule } from './module/product-category/product-category.module';
 import { ProductSubCategoryModule } from './module/product-sub-category/product-sub-category.module';
 
+import { join } from 'path';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 @Module({
   imports: [
-    ScheduleModule.forRoot(),  
-    MongooseModule.forRoot(process.env.URI,),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot:"/public"
+    }),
+    ScheduleModule.forRoot(),
+    MongooseModule.forRoot(process.env.URI),
     MongooseModule.forFeature(forFeatureDb),
-    UserModule,MailerModule,NotificationModule, VendorModule,
-    ProductModule, OrderModule, AuthModule, AdminModule,
-    CartModule, LogisticsModule, SavedItemsModule,
-    UWaveUserModule, UWaveAdminModule, FreightModule, 
-    RedisModule, ItemsModule, CountryCurrencyModule, ProductCategoryModule, ProductSubCategoryModule
-  ],
-  
-  providers:[
 
+    UserModule,
+    MailerModule,
+    NotificationModule,
+    VendorModule,
+    ProductModule,
+    OrderModule,
+    AuthModule,
+    AdminModule,
+    CartModule,
+    LogisticsModule,
+    SavedItemsModule,
+    UWaveUserModule,
+    UWaveAdminModule,
+    FreightModule,
+    RedisModule,
+    ItemsModule,
+    CountryCurrencyModule,
+    ProductCategoryModule,
+    ProductSubCategoryModule,
+  ],
+
+  providers: [
     // {
     //   provide: APP_FILTER,
     //   useClass: AllExceptionsFilter,
     // },
   ],
-  controllers: [HealthController]
+  controllers: [HealthController],
 })
+export class AppModule  {
 
-export class AppModule {}
+  // configure(app: NestExpressApplication) {
+  //   app.useStaticAssets(join(__dirname, '..', 'public'), {
+  //     prefix: '/static/', // This sets the URL prefix for accessing static files
+  //   });
+  // }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './helpers/exception';
+import { join } from 'path';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({}));
   app.useGlobalFilters(new AllExceptionsFilter());
+  // app.useStaticAssets(join(__dirname, '..', 'public'))
+
   app.enableCors({
     origin:true,
     // origin:  (origin, callback) => {
